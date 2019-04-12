1a7323c9c47df3433ae43a0fd4f4c5d12e29d591
Jesse Gjerde <jessegjerde89@gmail.com>
2019-04-12 16:37:34 -0500

    first commit

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ $(document).ready (readyNow)
 function readyNow(){
 
 $('#addEmployee').click('on', addClick );
-$('#removeEmployee'.click('on', removeClick ))
-)
+$('#removeEmployee').click('on', removeClick );
 }
 
 //Adds users to the dom
 function addClick(){
-    let fName = $.('#FName').value() 
-    let lName = $.('#LName').value()
-    let id = $.('#ID').value() 
-    let title = $.('#Title').value() 
-    let salary = $.('#ASalary').value() 
 
-    <tb id="tableBody"></tb>
+    const newEmployee = {
+        fName: $( '#FName' ).val(),
+        lName: $( '#LName' ).val(),
+        id: $( '#ID' ).val(),
+        title:  $( '#Title' ).val(),
+        salary: $( '#ASalary' ).val(),
+    }
 
-    $('#FName, #LName, #ID, #Title, #ASalary').empty(); 
+    //<tb id="tableBody"></tb>
+    console.log(newEmployee); 
+    $('#tableBody').push(newEmployee); 
+
+    $('#FName, #LName, #ID, #Title, #ASalary').val(''); 
 }
 
 //Removes users to the dom
