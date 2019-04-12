9f8baed9585f06dff8e960592f0c91090cb36c88
Jesse Gjerde <jessegjerde89@gmail.com>
2019-04-12 16:15:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready (readyNow)
+
+
+
+function readyNow(){
+
+$('#addEmployee').click('on', addClick );
+$('#removeEmployee'.click('on', removeClick ))
+)
+}
+
+//Adds users to the dom
+function addClick(){
+    let fName = $.('#FName').value() 
+    let lName = $.('#LName').value()
+    let id = $.('#ID').value() 
+    let title = $.('#Title').value() 
+    let salary = $.('#ASalary').value() 
+
+    <tb id="tableBody"></tb>
+
+    $('#FName, #LName, #ID, #Title, #ASalary').empty(); 
+}
+
+//Removes users to the dom
+function removeClick(){
+
+
+}
