7400dbddaf51384c7e814cf284f2578f99a989f2
Jesse Gjerde <jessegjerde89@gmail.com>
2019-04-13 11:29:51 -0500

    functional

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ $(document).ready(readyNow);
 
 let employee = []; 
 let salaryArray = []; 
+let monthlySalary = 0; 
+let sum = 0; 
 
 function readyNow(){
 
 $('#addEmployee').click('on', addClick );
 $('#removeEmployee').click('on', removeClick );
-showEmployee( employee )
+showEmployee( employee ); 
 }
 
 //Adds users to the dom
@@ -20,19 +22,35 @@ function addClick(){
         title:  $( '#Title' ).val(),
         salary: $( '#ASalary' ).val(),
     }
-
-    //<tb id="tableBody"></tb>
     
-   employee.push(newEmployee); 
-   
-   console.log(employee);
-   if (employee.fName =='' || employee.lName == '' || 
-      employee.id == '' || employee.title == '' || employee.salary == ''){
+   employee.push( newEmployee ); 
+   console.log(employee); 
+
+   for( let i = 0; i < employee.length; i++){
+        newSalaryValue = employee[i].salary; 
+   }
+   salaryArray.push(newSalaryValue); 
+
+   if (employee.fName == ' ' || employee.lName == ' ' || 
+      employee.id == ' ' || employee.title == ' ' || employee.salary == ' '){
         alert ( 'Please enter values for all fields'); 
       } else {
     showEmployee(employee); 
     $('#FName, #LName, #ID, #Title, #ASalary').val(''); 
       }
+
+       for (let i = 0; i < salaryArray.length; i++){
+           let el = parseInt(salaryArray[i]); 
+           console.log(el); 
+          sum += el; 
+           el = 0; 
+       }
+      
+       monthlySalary = Math.round(((sum / salaryArray.length) / 12)); 
+       console.log(monthlySalary); 
+       sum = 0; 
+     $('#monthly').empty(); 
+     $('#monthly').append('Total Monthy : ', monthlySalary ); 
 }
 
 function showEmployee( employees ){
@@ -45,11 +63,12 @@ function showEmployee( employees ){
            <td> ${ employees[i].id}</td>,
            <td> ${ employees[i].title}</td>, 
            <td> ${ employees[i].salary} </td> </tr> `;
-           
-           
+                
            newEmp.append(addedEmployee);
-           console.log(newEmp);
         }
+        
+      
+    
 } 
 
 //Removes users to the dom
